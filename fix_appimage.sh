6f1d3f877f17849a3e6bb35482c9e47ead5327f3
Chris Bier <github@cymor.com>
2023-03-31 10:50:16 -0500

    Switching to find; Fixing logic

--- fix_appimage.sh
+++ fix_appimage.sh
@@ -4,11 +4,11 @@ my_bin="/home/user/bin"
 
 app=$1
 
-new_appimage=$(ls "$my_bin"/"${app}""-*" | tail -n 1)
+new_appimage=$(find "$my_bin" -iname "*${app}*.AppImage" | tail -n 1)
 
-if [ ! -e "$app" ]
+if [ -e "$app" ]
 then
     rm "$app"
 fi
 
-ln -s "$my_bin"/"$new_appimage" "$my_bin"/"$app"
+ln -s "$new_appimage" "$my_bin"/"$app"
