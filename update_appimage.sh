b92a0b38040510e1b754c18d1e5d9b0ed9a8a74f
Chris Bier <github@cymor.com>
2023-03-31 10:18:51 -0500

    Fixing platform name; Moving numbered param to var

--- update_appimage.sh
+++ update_appimage.sh
@@ -2,20 +2,21 @@
 set -e
 
 repo=$1
-platform="amd64"
+symlink_name=$2
+platform="x86_64"
 my_bin="/home/user/bin"
 
 # Fetch latest release
 release_path=$(curl -s "https://github.com/$repo/releases" | grep 'releases/tag' | awk -F'"' '{print $6}' | awk -F'/' '{print $6}' | head -n 1)
-# echo "$release_path"
+echo "$release_path"
 
 appimage_link=$(curl -s "https://github.com/$repo/releases/expanded_assets/$release_path" | grep ${platform} | grep 'AppImage"' | awk -F'"' '{print $2}' | grep -v sha256 | head -n 1)
 file_name=$(echo "$appimage_link" | awk -F'/' '{print $7}')
 
 file_path="$my_bin"/"$file_name"
 
-# echo "$appimage_link" "$file_name"
+echo "$appimage_link" "$file_path"
 wget -O "$file_path" https://github.com/"$appimage_link"
 chmod +x "$file_path"
 
-"$my_bin"/fix_appimage.sh $2
+"$my_bin"/fix_appimage.sh "$symlink_name"
